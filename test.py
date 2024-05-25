3e6b2105c3f36eb01a061ea0cddda1e2c2a500da
dynstat <vs15vivek@gmail.com>
2024-05-26 01:51:36 +0530

    function error testing

--- test.py
+++ test.py
@@ -11,6 +11,11 @@ from wazirxapi.helpers import remove_unwanted_keys
 from time import sleep
 import threading
 
+from pprint import pprint
+
+INR_FOR_XRP = 200 # 200 INR available for XRP
+XRP_AMOUNT = 0
+
 live_market_data = {}
 last_placed_orders = {}
 
@@ -32,7 +37,16 @@ def update_market_data(symbols):
             else:
                 pass
             sleep(3) # to avoid rate limiting
-            print(f"Updated live_market_data for {symbol}: {live_market_data[ticker_data['symbol']]}")
+            if symbol in last_placed_orders:
+                print("----------------------------------------")
+                pprint(f"Updated live_market_data for {symbol}: ")
+                pprint(live_market_data[ticker_data['symbol']])
+                print("\n")
+                pprint(f"Last placed order for {symbol}: ")
+                pprint(last_placed_orders[symbol])
+                print("----------------------------------------\n\n\n")
+                
+          
         sleep(5)
 
 if __name__ == "__main__":
@@ -43,16 +57,27 @@ if __name__ == "__main__":
     # sleep(3)
     
     # Main thread continues with other tasks
-    # show_my_funds(wzx_api, funds_type="non_zero")
-    # show_open_orders(wzx_api, symbol="xrpinr")
+    show_my_funds(wzx_api, funds_type="non_zero")
+    open_orders_list = show_open_orders(wzx_api, symbol="xrpinr")
+    # print(open_orders_list)
+    
+    if open_orders_list:
+        for order in open_orders_list:
+            last_placed_orders.update({order['symbol']: remove_unwanted_keys(order)})
+            pprint(f"all open orders for {order['symbol']}: ")
+            pprint(last_placed_orders[order['symbol']])
+            print("\n")
+    else:
+        print("No open orders")
+    
     # # cancel_order(wzx_api, symbol="xrpinr", order_id="4482351439")
-    # order_details = place_order(
-    #     wzx_api, symbol="xrpinr", side="buy", order_type="limit", price=30, quantity=4
-    # )
+    # cancel_all_open_orders(wzx_api, symbol="xrpinr")
+    order_details = place_order(
+        wzx_api, symbol="xrpinr", side="buy", order_type="limit", price=30, quantity=4
+    )
     # placed_order_item = remove_unwanted_keys(order_details)
     # last_placed_orders.update({placed_order_item["symbol"]: placed_order_item})
 
-    # cancel_all_open_orders(wzx_api, symbol="xrpinr")
 
     # Optionally, join the thread if you need to wait for it to finish
     # Wait for the thread to finish
